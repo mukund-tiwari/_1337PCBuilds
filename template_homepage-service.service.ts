0501f02555c8c54fe6076ad9a745573038bcb769
Mukund Tiwari <mukundtiwari@outlook.com>
2021-08-16 11:05:51 +0530

    angular service template provided

---
+++ template_homepage-service.service.ts
@@ -0,0 +1,25 @@
+﻿////import { Injectable } from '@angular/core';
+////import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+////import { Observable, throwError } from 'rxjs';
+////import { catchError } from 'rxjs/operators';
+////import { ICustomerPcbuildDetail } from 'src/app-interfaces/ICustomerPcbuildDetail';
+
+////@Injectable({
+////    providedIn: 'root'
+////})
+////export class HomepageServiceService {
+
+////    constructor(private http: HttpClient) { }
+
+////    addPCBuildDetails(Name: string, EmailId: string, UseCase: string, Budget: string, AdditionalNotes: string): Observable<boolean> {
+////        var obj: ICustomerPcbuildDetail;
+////        obj = { Name: Name, EmailId: EmailId, UseCase: UseCase, Budget: Budget, AdditionalNotes: AdditionalNotes };
+////        var response = this.http.post<boolean>('/api', obj)
+////            .pipe(catchError(this.errorHandler));
+////        return response;
+////    }
+
+////    errorHandler(error: HttpErrorResponse) {
+////        return throwError(error.message || 'ERROR')
+////    }
+////}
