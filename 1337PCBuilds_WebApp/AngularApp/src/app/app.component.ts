b6bb5f9a6a9aa2b481f8e6c98a37c4f77d48f7cc
Shashank Sharda <64969681+shashanksharda98@users.noreply.github.com>
2021-08-22 10:02:53 +0530

    form remaining

--- 1337PCBuilds_WebApp/AngularApp/src/app/app.component.ts
+++ 1337PCBuilds_WebApp/AngularApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { HomepageServiceService } from '../app-services/homepage-service.service';
 import { ICustomerPcbuildDetail } from '../app-interfaces/ICustomerPcbuildDetail';
+import { HomepageServiceService } from './app-services/homepage-service.service';
 
 @Component({
   selector: 'app-root',
@@ -18,30 +18,8 @@ export class AppComponent implements OnInit{
   constructor(private _homepageService: HomepageServiceService) { }
   ngOnInit() { }
   AddDetails(form: NgForm) {
-    //console.log(form.value.name);
-    //console.log(form.value.email);
-    //console.log(form.value.UseCase);
-    //console.log(form.value.Budget);
-    //console.log(form.value.AdditionalNotes);
+    
 
-    //console.log('Easter Egg');
-    this._homepageService.addPCBuildDetails(form.value.name, form.value.email, form.value.UseCase, form.value.Budget,
-      form.value.AdditionalNotes).subscribe(
-        response => {
-          this.status = response;
-          if (response) {
-            alert("Details submitted successfully. \n\nThis is an experimental website.\n\nFor business enquiries, contact us on WhatsApp at:\n+91-8368490306");
-            form.reset();
-          } else {
-            alert("Sorry, there is some error. Please verify your details. Make sure the Email-Id is unique.")
-            form.reset();
-          }
-        },
-        responseError => {
-          alert("Sorry, there is some error. Please verify your details.")
-          form.reset();
-        },
-        () => console.log("Easter Egg.")
-      );
+   
   }
 }
--- 1337PCBuilds_WebApp/AngularApp/src/app/app.module.ts
+++ 1337PCBuilds_WebApp/AngularApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
